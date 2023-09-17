0a90b44abfeb858a5fd47fb1c90ef1e01933e193
mody100000 <m.anamody10000@gmail.com>
2023-09-17 05:00:58 +0300

    fetch the games

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,35 @@
+import { Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import apiClient from "../services/apiClient";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FatchGamesResponse {
+  count: number;
+  results: Game[];
+}
+const GameGrid = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    apiClient
+      .get<FatchGamesResponse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  });
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GameGrid;
