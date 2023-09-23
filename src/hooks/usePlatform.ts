559d8542ced5aaccebe2beacf5eb468899dadd63
mody100000 <m.anamody10000@gmail.com>
2023-09-23 05:23:52 +0300

    customize the themes to get a darker grays

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,13 +1,10 @@
-import useData from "./useData";
-
+import platforms from "../data/platforms";
 interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatform = () => {
-  return useData<Platform>("/platforms/lists/parents");
-};
+const usePlatform = () => ({ data: platforms, error: null });
 
 export default usePlatform;
